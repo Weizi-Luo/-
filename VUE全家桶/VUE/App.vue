da1472858cebce49a31f51eeed30148430f50fba
WeiZi <1394073064@qq.com>
2019-06-04 21:30:47 +0800

    vue初探

---
+++ VUE全家桶/VUE/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>i am a jiejie</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: 'hello world'
+        }
+    }
+}
+</script>
+
+<style>
+    div {
+        background-color:red;
+        color:#fff;
+    }
+</style>
+
+
